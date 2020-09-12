650f341dc528999fcadb492fe1a2c108db9fbdf1
mumtaj <mumtaj@gmail.com>
2020-09-12 15:54:43 +0530

    Update signup.php

--- web/signup.php
+++ web/signup.php
@@ -18,7 +18,7 @@ $email->addTo("mumtaj@gmail.com", "Mumtaj Pathan");
 extract($_REQUEST);
 $email->addContent(
   "text/html", 
-  "Dear User,<p>Congratulations! New user has signed up. The details are as under:</p>".
+  "Dear User,<p></p><p>Congratulations! New user has signed up. The details are as under:</p>".
   "<p><table border=0 padding=5><tr><td>Name:</td><td>$name</td></tr><tr><td>Email:</td><td>$email</td></tr>".
   "<tr><td>Gender:</td><td>$gender</td></tr><tr><td>Date of Birth:</td><td>$dob</td></tr></table></p>".
   "<p></p><p>Regards,</p><p><strong>Team 4AM</strong></p>"
