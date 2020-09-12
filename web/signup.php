2929404d6c3e0c680f25dd81dd96c512096efd50
mumtaj <mumtaj@gmail.com>
2020-09-12 16:54:54 +0530

    Update signup.php

--- web/signup.php
+++ web/signup.php
@@ -1,6 +1,10 @@
 <?php
 
-print_r($_REQUEST);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// print_r($_REQUEST);
 extract($_REQUEST);
 function getMailContent($_REQUEST) {
   extract($_REQUEST);
@@ -24,17 +28,12 @@ $mail = new \SendGrid\Mail\Mail();
 $mail->setFrom("webmobiletechie@gmail.com", "Web Mobile Techie");
 // $mail->setFrom("test@example.com", "Example User");
 $mail->setSubject("[4am] New User Signup");
-$mail->addTo($email, $name);
+$mail->addTo($_REQUEST["email"], $_REQUEST["name"]);
 // $mail->addContent(
 //     "text/plain", "and easy to do anywhere, even with PHP"
 // );
-$mail->addContent(
-  "text/html", 
-  "Dear User,<p></p><p>Congratulations! You have successfully subscribed for 4am-worldwide newsletter. Your details are as under:</p>".
-  "<p><table border=0 padding=5><tr><td>Name:</td><td>$name</td></tr><tr><td>Email:</td><td>$email</td></tr>".
-  "<tr><td>Gender:</td><td>$gender</td></tr><tr><td>Date of Birth:</td><td>$dob</td></tr></table></p>".
-  "<p></p><p>Regards,</p><p><strong>Team 4AM</strong></p>"
-);
+$mail->addContent("text/html", getMailContent($_REQUEST));
+
 $sendgrid = new \SendGrid(SENDGRID_API_KEY);
 try {
     $response = $sendgrid->send($mail);
